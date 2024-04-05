e1e39e9e799cc1867d27599c7cd40bc509e50a7d
PisarevskiyRE <daimon--@live.ru>
2024-04-06 00:15:09 +0300

    part 4 13-27

--- src/main/scala/ProgramOld.scala
+++ src/main/scala/ProgramOld.scala
@@ -5,20 +5,12 @@ import scala.annotation.tailrec
 object ProgramOld {
 
   //  def creteDescription(args: Array[String]): IO[Unit] =
-  //    displayKleisli(hyphens).flatMap { _ =>
-  //      displayKleisli(question).flatMap { _ =>
-  //        promptKleisli.flatMap { input =>
-  //          IO.create {
-  //            val integerAmount: Int = convertStringToInt(input)
-  //            val positiveAmount: Int = ensureAmountPositive(integerAmount)
-  //            val balance: Int = round(positiveAmount)
-  //            val message: String = createMessage(balance)
-  //            message
-  //          }.flatMap { message =>
-  //            displayKleisli(message).flatMap { _ =>
-  //              displayKleisli(hyphens).map { _ =>
-  //                ()
-  //              }
+  //    display(hyphens).flatMap { _ =>
+  //      display(question).flatMap { _ =>
+  //        prompt.flatMap { input =>
+  //          display(fromInputToMessage(input)).flatMap { _ =>
+  //            display(hyphens).map { _ =>
+  //              ()
   //            }
   //          }
   //        }
@@ -26,54 +18,67 @@ object ProgramOld {
   //    }
 
   def creteDescription(args: Array[String]): IO[Unit] = for {
-    _ <- displayKleisli(hyphens)
-    _ <- displayKleisli(question)
-    input <- promptKleisli
+    _ <- display(hyphens)
+    _ <- display(question)
+    input <- prompt
 
-    integerAmount = convertStringToInt(input)
-    positiveAmount = ensureAmountPositive(integerAmount)
-    balance = round(positiveAmount)
-    message = createMessage(balance)
-
-    _ <- displayKleisli(message)
-    _ <- displayKleisli(hyphens)
+    _ <- display(fromInputToMessage(input))
+    _ <- display(hyphens)
   } yield ()
 
-  def associativityLaw = {
-    type AnythingThatHasMonad[A] = IO[A]
-    type AnyType = Int
-    val anyValue: AnyType = 5
+  private def fromInputToMessage(input: String): String = {
 
-    val io: AnythingThatHasMonad[AnyType] = IO.create(anyValue)
-    val f: AnyType => AnythingThatHasMonad[String] = a => IO.create(a.toString)
-    val g: String => AnythingThatHasMonad[Unit] = a => IO.create(println(a))
+    val maybeInteger: Maybe[Int] = convertStringToInt(input)
 
-    io.flatMap(f).flatMap(g) == io.flatMap(a => f(a).flatMap(g))
-    io.flatMap(f).flatMap(g) == io.flatMap(f >=> g)
+    val message: String = maybeInteger match {
+      case Maybe.Just(integerAmount) =>
+        val positiveAmount = ensureAmountPositive(integerAmount)
+        val balance = round(positiveAmount)
+        val message = createMessage(balance)
 
-    val f1: AnyType => String = a => a.toString
-    val g1: String => Unit = a => println(a)
+        message
+      case Maybe.Nothing =>
+        val message = "}{y"
 
-    io.map(f1).map(g1) == io.map(a => g1(f1(a)))
-    io.map(f1).map(g1) == io.map(f1 --> g1)
+        message
+    }
 
+    message
   }
 
-  def identityLaw = {
-    type AnythingThatHasMonad[A] = IO[A]
-    type AnyType = Int
-    val anyValue: AnyType = 5
-
-    val M = Monad[IO]
-    val f: AnyType => AnythingThatHasMonad[String] = a => M.pure(a.toString)
-
-    val io = M.pure(anyValue)
-    M.flatMap(io)(f) == io
-    io.flatMap(f) == io
-    f(anyValue).flatMap(a => IO.create(a)) == IO.create(anyValue)
-
-  }
-
+  //  def associativityLaw = {
+  //    type AnythingThatHasMonad[A] = IO[A]
+  //    type AnyType = Int
+  //    val anyValue: AnyType = 5
+  //
+  //    val io: AnythingThatHasMonad[AnyType] = IO.create(anyValue)
+  //    val f: AnyType => AnythingThatHasMonad[String] = a => IO.create(a.toString)
+  //    val g: String => AnythingThatHasMonad[Unit] = a => IO.create(println(a))
+  //
+  //    io.flatMap(f).flatMap(g) == io.flatMap(a => f(a).flatMap(g))
+  //    io.flatMap(f).flatMap(g) == io.flatMap(f >=> g)
+  //
+  //    val f1: AnyType => String = a => a.toString
+  //    val g1: String => Unit = a => println(a)
+  //
+  //    io.map(f1).map(g1) == io.map(a => g1(f1(a)))
+  //    io.map(f1).map(g1) == io.map(f1 --> g1)
+  //
+  //  }
+  //
+  //  def identityLaw = {
+  //    type AnythingThatHasMonad[A] = IO[A]
+  //    type AnyType = Int
+  //    val anyValue: AnyType = 5
+  //
+  //    val M = Monad[IO]
+  //    val f: AnyType => AnythingThatHasMonad[String] = a => M.pure(a.toString)
+  //
+  //    val io = M.pure(anyValue)
+  //    M.flatMap(io)(f) == io
+  //    io.flatMap(f) == io
+  //    f(anyValue).flatMap(a => IO.create(a)) == IO.create(anyValue)
+  //  }
 
   private val hyphens: String =
     "\u2500" * 50
@@ -81,22 +86,21 @@ object ProgramOld {
   private val question: String =
     "Some text"
 
-  private def display(input: Any): Unit =
-    println(input)
-
-  private def displayKleisli(input: Any): IO[Unit] = {
+  private def display(input: Any): IO[Unit] = {
     IO.create {
       println(input)
     }
   }
 
-  private def promptKleisli: IO[String] = IO.create("5")
+  private def prompt: IO[String] = IO.create("5")
+  //scala.io.StdIn.readLine()
 
-  private def prompt(): String =
-    scala.io.StdIn.readLine()
-
-  private def convertStringToInt(input: String): Int =
-    input.toInt
+  private def convertStringToInt(input: String): Maybe[Int] = {
+    try Maybe.Just(input.toInt)
+    catch {
+      case _: NumberFormatException => Maybe.Nothing
+    }
+  }
 
   private def ensureAmountPositive(amount: Int): Int =
     if (amount < 1)
